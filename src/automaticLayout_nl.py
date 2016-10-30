e738dba4ea104c3249409e04644673bc63cf2622
petrvanblokland <buro@petr.com>
2016-10-30 11:29:37 -0400

    Transformation that elements do not know their position.
    This allows to placement of element in multiple positions.
    And if gives the page and composer better control on the what is filled
    on the page.
    (not working yet)

--- src/automaticLayout_nl.py
+++ src/automaticLayout_nl.py
@@ -6,40 +6,18 @@ import xml.etree.ElementTree as ET
 
 import documentG # Temp different from document, while developing Galley
 reload(documentG)
-from documentG import Document, Page, Composer, Template, Galley
+from documentG import Document, Page, Composer, Template, Galley, getRootStyle
 
+FILENAME = 'automaticLayout_nl.md' # 'automaticLayout_nl.md'
+TITLE = 'Automatic Layout'
 
-# Basic layout measures 
-U = 7
-PW = 595 # Page width 210mm, international generic fit.
-PH = 11 * 72 # Page height 11", international generic fit.
-ML = 7*U # Margin left
-MT = 7*U # Margin top
-BASELINE_GRID = 2*U
-CW = 11*U # Column width. 
-G = U # Generic gutter.
-CH = 6*BASELINE_GRID - G # Approx. square. Fit with baseline.
-Utab = U*0.8 # Indent for bullet lists
-# Display option
-SHOW_GRID = True
-SHOW_BASELINEGRID = True
-# Text measures
-LEADING = BASELINE_GRID
-BODYTEXT_SIZE = 10
-# Tracking presets
+# ELement id of the text box on pages the hold the main text flow.
+MAIN_FLOW = 'main' 
+
+# Tracking presets for tag styles.
 H1_TRACK = H2_TRACK = 0.015
 H3_TRACK = 0.030 # Tracking as relative factor to font size.
 P_TRACK = 0.030
-# Language settings
-LANGUAGE = 'nl-be'
-MAIN_FLOW = 'main' # ELement id of the text box on pages the hold the main text flow.
-FILENAME = 'automaticLayout_nl.md' # 'automaticLayout_nl.md'
-PAGENUMBER = '#?#'
-NO_COLOR= -1
-BOX_COLOR = NO_COLOR #0.9 # Debug color for textbox columns.
-MISSING_IMAGE_FILL = 0.5
-JUSTIFIED = 'justified'
-LEFT = 'left'
 
 if 1:
     BOOK = 'Productus-Book'
@@ -51,6 +29,33 @@ else:
     BOOK = MEDIUM = 'Georgia'
     BOOK_ITALIC = 'Georgia-Italic'
     BOLD = SEMIBOLD = 'Georgia-Bold'
+
+rs = getRootStyle()
+
+# Alter basic layout measures 
+rs.u = U = 7
+rs.pw = 595 # Page width 210mm, international generic fit.
+rs.ph = 11 * 72 # Page height 11", international generic fit.
+rs.ml = 7*U # Margin left
+rs.mt = 7*U # Margin top
+rs.baselineGrid = 2*U
+rs.cw = 11*U # Column width. 
+rs.g = U # Generic gutter.
+rs.ch = rs.baselineGrid - rs.g # Approx. square. Fit with baseline.
+rs.listTabs = [(U*0.8, 'left')] # Default indent for bullet lists
+# Display option
+rs.gridStroke = 0.8
+rs.showGrid = True
+rs.showBaselineGrid = True
+# Text measures
+rs.leading = rs.baselineGrid
+rs.fontSize = 10
+rs.font = BOOK
+# Language settings
+rs.language = 'nl-be'
+
+BOX_COLOR = rs.noColor # Optonal fill color to mark columns.
+
 # -----------------------------------------------------------------         
 def makeDocument():
     u"""Demo page composer."""
@@ -62,12 +67,13 @@ def makeDocument():
     flowId2 = MAIN_FLOW+'2'
     
     # Template 1
-    template1 = Template(PW, PH) # Create template of main size. Front page only.
-    if SHOW_GRID: # Enable to show grid columns and margins.
+    template1 = Template(rs.w, rs.h, rs) # Create template of main size. Front page only.
+    if rs.showGrid: # Enable to show grid columns and margins.
         template1.grid() 
-    if SHOW_BASELINEGRID: # Enable to show baseline grid.
+    if rs.showBaselineGrid: # Enable to show baseline grid.
         template1.baselineGrid()
     # Create empty image place holders. To be filled by running content on the page.
+    # Since there is no style yet, templates cannot use cImage, etc.
     template1.cImage(None, 4, 0, 2, 4) 
     template1.cImage(None, 0, 5, 2, 3)
     # Create linked text boxes. Note the "nextPage" to keep on the same page or to next.
@@ -75,13 +81,13 @@ def makeDocument():
     template1.cTextBox('', 2, 0, 2, 8, flowId1, nextBox=flowId2, nextPage=0, fill=BOX_COLOR)
     template1.cTextBox('', 4, 4, 2, 4, flowId2, nextBox=flowId0, nextPage=1, fill=BOX_COLOR)
     # Create page number box. Pattern PAGENUMBER is replaced by actual page number.
-    template1.cText(PAGENUMBER, 6, 0, font=BOOK, fontSize=12, fill=BOX_COLOR)
+    template1.cText(rs.pageNumberMarker, 6, 0, font=rs.font, fontSize=12, fill=BOX_COLOR)
 
     # Template 2
-    template2 = Template(PW, PH) # Create second template. This is for the main pages.
-    if SHOW_GRID: # Enable to show grid columns and margins.
+    template2 = Template(rs.w, rs.h, rs) # Create second template. This is for the main pages.
+    if rs.showGrid: # Enable to show grid columns and margins.
         template2.grid() 
-    if SHOW_BASELINEGRID: # Enable to show baseline grid.
+    if rs.showBaselineGrid: # Enable to show baseline grid.
         template2.baselineGrid()
     template2.cImage(None, 4, 0, 2, 3)
     template2.cImage(None, 0, 5, 2, 3)
@@ -92,46 +98,46 @@ def makeDocument():
     template2.cTextBox('', 2, 4, 2, 4, flowId1, nextBox=flowId2, nextPage=0, fill=BOX_COLOR)
     template2.cTextBox('', 4, 3, 2, 3, flowId2, nextBox=flowId0, nextPage=1, fill=BOX_COLOR)
     # Create page number box. Pattern PAGENUMBER is replaced by actual page number.
-    template2.cText(PAGENUMBER, 6, 0, font=BOOK, fontSize=12, fill=BOX_COLOR)
+    template2.cText(rs.pageNumberMarker, 6, 0, font=rs.font, fontSize=12, fill=BOX_COLOR)
    
     # Create new document with (w,h) and fixed amount of pages.
     # Make number of pages with default document size.
     # Initially make all pages default with template2
-    doc = Document(PW, PH, ml=ML, mt=MT, cw=CW, ch=CH, g=G, 
-        gridStroke=0.8, pages=2, template=template2,
-        baselineGrid=BASELINE_GRID, 
-        missingImageFill=MISSING_IMAGE_FILL) 
+    doc = Document(rs, TITLE, template=template2, pages=2) 
      
-    # Add styles for whole document and text flows.               
+    # Add styles for whole document and text flows.  
+    doc.newStyle(name='document')
+    doc.newStyle(name='page')             
     doc.newStyle(name='chapter', font=BOOK)    
     doc.newStyle(name='title', fontSize=32, font=BOLD)
     doc.newStyle(name='subtitle', fontSize=16, font=BOOK_ITALIC)
     doc.newStyle(name='author', fontSize=16, font=BOOK, fill=(1, 0, 0))
     doc.newStyle(name='h1', fontSize=20, font=SEMIBOLD, fill=0.1, stroke=None,
-        leading=20, rLeading=0, tracking=H1_TRACK, needsBelow=LEADING*3)
+        leading=20, rLeading=0, tracking=H1_TRACK, needsBelow=rs.needsBelow)
     doc.newStyle(name='h2', fontSize=16, font=SEMIBOLD, fill=0.2, stroke=None, 
-        leading=20, rLeading=0, tracking=H2_TRACK, needsBelow=LEADING*3)
+        leading=20, rLeading=0, tracking=H2_TRACK, needsBelow=rs.needsBelow)
     doc.newStyle(name='h3', fontSize=12, font=MEDIUM, fill=0, 
-        leading=15, rLeading=0, needsBelow=LEADING*3,
+        leading=15, rLeading=0, needsBelow=rs.needsBelow,
         tracking=H3_TRACK,
         paragraphTopSpacing=U, paragraphBottomSpacing=U/2)
     
     # Spaced paragraphs.
-    doc.newStyle(name='p', fontSize=BODYTEXT_SIZE, font=BOOK, fill=0.1, 
-        stroke=None, tracking=P_TRACK, language=LANGUAGE, align=LEFT,
-        leading=BASELINE_GRID, rLeading=0, hyphenation=True, 
+    doc.newStyle(name='p', fontSize=rs.fontSize, font=BOOK, fill=0.1, 
+        stroke=None, tracking=P_TRACK, language=rs.language, align=rs.align,
+        leading=rs.leading, rLeading=rs.rLeading, hyphenation=rs.hyphenation, 
         stripWhiteSpace=' ')
     doc.newStyle(name='b', font=SEMIBOLD, stripWhiteSpace=' ')
     doc.newStyle(name='em', font=BOOK_ITALIC, stripWhiteSpace=' ')
-    doc.newStyle(name='img', leading=BASELINE_GRID, rLeading=0,
-        fontSize=BODYTEXT_SIZE, font=BOOK,)
+    doc.newStyle(name='img', leading=rs.leading, rLeading=rs.rLeading,
+        fontSize=rs.fontSize, font=BOOK,)
     
     # Footnote reference index.
     doc.newStyle(name='sup', font=MEDIUM, 
          rBaselineShift=0.6, fontSize=14*0.65, stripWhiteSpace=' ')
-    doc.newStyle(name='li', fontSize=BODYTEXT_SIZE, font=BOOK, 
-        tracking=P_TRACK, leading=BASELINE_GRID, rLeading=0, hyphenation=True, tabs=[(Utab, 'left')], 
-        indent=Utab, firstLineIndent=1, #tailIndent=U, 
+    doc.newStyle(name='li', fontSize=rs.fontSize, font=BOOK, 
+        tracking=P_TRACK, leading=rs.leading, rLeading=rs.rLeading,
+        hyphenation=True, indent=rs.listIndent, firstLineIndent=1, 
+        #tailIndent=U, 
         stripWhiteSpace=' ')
     doc.newStyle(name='ul', stripWhiteSpace=' ',)
     doc.newStyle(name='literatureref', stripWhiteSpace=False,
@@ -139,9 +145,10 @@ def makeDocument():
     doc.newStyle(name='footnote', stripWhiteSpace=False,
         fill=(1, 0, 0), fontSize=0.8*U, font=BOOK)
     doc.newStyle(name='caption', stripWhiteSpace=False, tracking=P_TRACK, 
-        language=LANGUAGE, 
-        fill=0.2, leading=BASELINE_GRID*0.8, fontSize=BODYTEXT_SIZE*0.8,
-        font=BOOK_ITALIC, indent=U/2, tailIndent=-U/2, hyphenation=True)
+        language=rs.language, 
+        fill=0.2, leading=rs.leading*0.8, fontSize=rs.fontSize*0.8,
+        font=BOOK_ITALIC, indent=rs.u/2, tailIndent=-rs.u/2, 
+        hyphenation=rs.hyphenation)
 
     # Create main Galley for this page, for pasting the sequence of elements.    
     g = Galley() 
